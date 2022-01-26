f43e19a683c495f6f938def140b11433fcaadfec
poyu <yoyohihirobert@gmail.com>
2022-01-27 00:22:01 +0800

    day5 calculator practice

---
+++ day5.py
@@ -0,0 +1,13 @@
+#create a simple calculater
+name = input("input your name: ")
+age = input("input your age: ")
+print("Hello, "+ name + ". your age is " + age)
+
+number1 = input("please text number1: ")
+number2 = input("please test number2: ")
+
+print(float(number1) + float(number2))
+
+
+
+
