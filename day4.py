429234b16d02e998337486fb122a5b58994cf6ab
poyu <yoyohihirobert@gmail.com>
2022-01-26 23:56:31 +0800

    day4 number practice

---
+++ day4.py
@@ -0,0 +1,18 @@
+from math import *
+print(8+5)
+print(8-5)
+print(8*5)
+print(8/5)
+print(8+8*5)
+number = 8
+print(number % 5)
+print("print "+str(number))
+# print("print "+ 8)
+print(pow(2, 4))
+print(max(2, 4, 5, 3, 88))
+print(round(5.1))
+print(floor(5.8))
+print(ceil(5.1))
+print(sqrt(64))
+print(sqrt(63))
+
